28ad4eb7e428c285ec98f4415b57489be916fb13
Conor <conorkenahan@gmail.com>
2025-06-23 13:05:58 -0400

    fixed prisma, added New button/page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,25 @@
-import Image from "next/image";
+import { prisma } from "@/db";
+import Link from "next/link";
 
-export default function Home() {
-  return <h1>Todo App</h1>
+export default async function Home() {
+  const todos = await prisma.todo.findMany();
+
+  return (
+    <>
+      <header className="flex justify-between items-center mb-4">
+        <h1 className="text-2xl">Todos</h1>
+        <Link
+          className="border border-slate-300 text-slate-300 px-2 py-1 rounded hover:bg-slate-700 focus-within:bg-slate-700 outline-none"
+          href="/new"
+        >
+          New
+        </Link>
+      </header>
+      <ul className="pl-4">
+        {todos.map((todo) => (
+          <li key={todo.id}>{todo.title}</li>
+        ))}
+      </ul>
+    </>
+  );
 }
